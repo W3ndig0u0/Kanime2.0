5e8cf9e9ac3374c8f024090884182f139d89ba42
W3ndig0 <Jingxiang304@gmail.com>
2023-03-28 01:54:20 +0200

    nice

--- Js/TopAiring.js
+++ Js/TopAiring.js
@@ -1,5 +1,6 @@
 function getTopAiring(page) {
   // let animeId = sessionStorage.getItem("AnimeID");
+  // const url = "https://api.consumet.org/anime/gogoanime/recent-episodes?page=" + page;
   const url = "https://api.consumet.org/anime/gogoanime/top-airing?page=" + page;
   
   fetch(url)
@@ -15,11 +16,11 @@ function ShowTopAiring(result) {
   console.log(result.results);
 
   const animeInfo = document.createElement("div");
-  animeInfo.className = "ImgRow";
+  animeInfo.className = "imgRow";
 
   for (let i = 0; i < result.results.length; i++) {
 
-  const animeEpisodes= document.createElement("div");
+  const animeEpisodes= document.createElement("span");
 
   const animeTitle = result.results[i].title;
   const animeId = result.results[i].id
@@ -27,19 +28,21 @@ function ShowTopAiring(result) {
   const genres = JSON.stringify(result.results[i].genres);
 
   const AnimeInfoInnerHTML = `
-  <div class="${animeId} animeCard" onclick="onClickAnime(this.className)">
+  <div id="${animeId}" class="animeCard" onclick="onClickAnime(this.id)">
   <h1>${animeTitle}</h1>
-  <div></div>
   <image src="${image}">
   <p>${animeId}</p>
   <p>${removeSign(genres)}</p>
   </div>
   `;
-  
+
   animeEpisodes.innerHTML = AnimeInfoInnerHTML;
   animeInfo.appendChild(animeEpisodes);
+  animeEpisodes.className = "animeCardHolder";
   document.querySelector(".animeTopAiring")?.appendChild(animeInfo);
   }
 }
 
+
+getTopAiring(2);
 getTopAiring(1);
